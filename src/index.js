9afb9427f79e797655a437f112b6113448b98082
andoss <anditahiri460@gmail.com>
2024-08-20 15:03:40 +0200

    Working writing and retriving data from Local Storage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-// index.js
-import { greeting } from "./greeting.js";
 
-console.log(greeting);
+
+const del = document.getElementById("del");
+const test = document.getElementById("test");
+const input = document.getElementById("input")
+
+del.addEventListener("click",show)
+test.addEventListener("click",()=>{
+    addItem(input.value)
+    input.value = ""
+})
+
+
+function show (){
+    // for(let i = 0; i < Object.keys(localStorage).length ; i++){
+    //     console.log(localStorage.getItem(Object.keys(localStorage)))
+    // }
+    // console.log(localStorage.getItem(Object.keys(localStorage)[i]))
+    for(let i = 0; i< localStorage.length; i++){
+        console.log(Object.keys(localStorage)[i])
+    }
+}
+
+function addItem (value) {
+    // if (parseInt(Object.keys(localStorage).length+1) > parseInt(Object.keys(localStorage)[Object.keys(localStorage).length-1])) {
+    //     localStorage.setItem(Object.keys(localStorage).length+1,value)
+    // }else{
+    //     localStorage.setItem(Object.keys(localStorage)[(Object.keys(localStorage).length-1)+1],value)
+    // }
+    input.value === "" ? alert("Enter something.."):
+    localStorage.setItem(value,"")
+    console.log(Object.keys(localStorage).length)
+}
---
+++ src/loaders.js
@@ -0,0 +1,13 @@
+
+const myArr = [1,2,"a", "asdds",4]
+
+// export const greeting = localStorage.list = JSON.stringify(myArr);
+
+class item {
+    constructor(name,status) {
+
+        name = this.name
+        status = this.status
+        
+    }
+}
