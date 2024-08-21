0fb59167d1545f9b925fc2dba1c3ab633e9645ab
andoss <anditahiri460@gmail.com>
2024-08-21 12:49:22 +0200

    CRD update & layout fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // import { starter } from "./loaders.js";
-
+import { starter,addItem,show } from "./loaders.js";
 
 const input = document.getElementById("input")
-const sub = document.getElementById("sub");
+// const sub = document.getElementById("sub");
 
 const add = document.getElementById("add");
 const del = document.getElementById("del")
@@ -10,6 +10,7 @@ const del = document.getElementById("del")
 const content = document.getElementById("content")
 
 starter()
+
 del.addEventListener("click",()=>{
     localStorage.clear()
     input.value = ""
@@ -17,7 +18,7 @@ del.addEventListener("click",()=>{
     starter()
 });
 
-sub.addEventListener("click",show)
+// sub.addEventListener("click",show)
 
 add.addEventListener("click",()=>{
     addItem(input.value)
@@ -25,68 +26,17 @@ add.addEventListener("click",()=>{
     show()
 })
 
-
-function show (){
-
-    content.innerHTML = "";
-
-    for(let i = 1; i < localStorage.length; i++){
-        const div = document.createElement('div')
-        div.dataset.index = i;
-        div.className = "navo";
-        const index = document.createElement('p')
-        index.innerText = i
-
-        const item = document.createElement('p')
-        item.innerText = localStorage.getItem([i])
-
-        const btn = document.createElement('button')
-        btn.innerText = 'Delete'
-        btn.addEventListener('click',()=>{
-            remove(i)
-        })
-
-        div.appendChild(index)
-        div.appendChild(item)
-        div.appendChild(btn)
-        content.appendChild(div)
+input.addEventListener("keypress",function(event){
+    if (event.key ==="Enter") {
+        
+        addItem(input.value);
+        input.value = ""
+        show()
     }
+})
+
+
+function edit (){
+
 }
-
-
-function starter(){
-    parseInt(localStorage.getItem('%x')) > 1 
-    ? show()
-    :localStorage.setItem("%x",1)
-}
-
-
-function addItem (value) {
-
-    if(value === ""){
-        alert("Enter something..")
-    }else{
-        localStorage.setItem(localStorage.getItem('%x'),value);
-
-        let x = parseInt(localStorage.getItem('%x'))+1
-        localStorage.setItem('%x',x)
-    }        
-}
-
-
-
-//to be exported
-
-
-function remove(index){
-
-    for(;index < localStorage.length-1; index++){
-     localStorage.setItem(index,localStorage.getItem(index+1))
-     console.log(index)
-    }
-    localStorage.removeItem(index)
-    localStorage.setItem('%x',index)
-    show()
-
-} 
 
--- src/loaders.js
+++ src/loaders.js
@@ -1,9 +1,85 @@
+export function starter(){
+    if(parseInt(localStorage.getItem('%x')) > 1 ){
+        show()
+        pretty(2)
+    }else{
+        localStorage.setItem("%x",1)
+        pretty(0)
+    }
+}
+function remove(index){
 
+    for(;index < localStorage.length-1; index++){
+     localStorage.setItem(index,localStorage.getItem(index+1))
+    }
+    localStorage.removeItem(index)
+    localStorage.setItem('%x',index)
+    show()
+} 
 
-// export const greeting = localStorage.list = JSON.stringify(myArr);
+export function addItem (value) {
 
-// export function starter(){
-//     parseInt(localStorage.getItem('%x')) > 1 
-//     ? show()
-//     :localStorage.setItem("%x",1)
-// }
+    if(value === ""){
+        alert("Enter something..")
+    }else{
+        localStorage.setItem(localStorage.getItem('%x'),value);
+
+        let x = parseInt(localStorage.getItem('%x'))+1
+        localStorage.setItem('%x',x)
+    }        
+}
+
+export function show (){
+
+    pretty(2)
+    content.innerHTML = "";
+
+    for(let i = 1; i < localStorage.length; i++){
+        const div = document.createElement('div')
+        div.dataset.index = i;
+        div.className = "navo";
+
+        const container = document.createElement('div')
+        container.classList.add('con')
+        const index = document.createElement('p')
+        index.innerText = i
+
+        const item = document.createElement('p')
+        item.innerText = localStorage.getItem([i])
+
+        const react = document.createElement('div')
+        react.classList.add('buttons')
+
+        const check = document.createElement('button')
+        check.innerHTML = '&#x2714;'
+        check.addEventListener('click',()=>{
+            checked(i)
+        })
+
+        const del = document.createElement('button')
+        del.innerText = 'Delete'
+        del.addEventListener('click',()=>{
+            remove(i)
+        })
+
+        container.appendChild(index)
+        container.appendChild(item)
+        div.appendChild(container)
+        react.appendChild(check)
+        react.appendChild(del)
+        div.appendChild(react)
+        content.appendChild(div)
+    }
+}
+
+export function checked(index){
+    let btn = document.querySelector(`[data-index="${index}"]`)
+    btn.classList.contains("checked")? btn.classList.remove("checked"): btn.classList.add("checked")
+}
+
+function pretty(i){
+    const x = document.querySelector('.search')
+    
+    i>1 ? x.classList.remove('bottom') : x.classList.add('bottom')
+
+}
